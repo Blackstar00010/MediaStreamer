6ac4212b4c8a6fe9cb169db1255be6e3f8a05871
Blackstar00010 <djwjames1@gmail.com>
2025-02-22 12:17:16 +0000

    fe change for the previous be change

--- src/pages/AlbumPage.jsx
+++ src/pages/AlbumPage.jsx
@@ -4,6 +4,7 @@ import { useParams, useNavigate } from "react-router-dom";
 const AlbumPage = ({ setCurrentSongID }) => {
     const { albumID } = useParams();
     const [albumName, setAlbumName] = useState("");
+    const [artistName, setArtistName] = useState("");
     const [albumArt, setAlbumArt] = useState("");
     const [tracks, setTracks] = useState([]);
     const navigate = useNavigate(); // Redirect to 404 if album not found
@@ -24,31 +25,48 @@ const AlbumPage = ({ setCurrentSongID }) => {
                 // console.log(data);
                 if (data) {
                     setAlbumName(data.album_name);
+                    setArtistName(data.album_artists);
                     setAlbumArt(data.album_art);
                     setTracks(data.tracks);
                 }
             })
         // .catch(() => navigate("/404"));
     }, [albumID, navigate]);
-    // console.log(albumArt);
+
+    console.log(tracks);
+
+    // sort by track number
+    tracks.sort((a, b) => a.track_number - b.track_number);
 
     return (
         <div style={styles.container}>
-            <h1>{albumName}</h1>
             <div style={styles.albumContents}>
-                <div className="album-art-container" style={styles.albumArtContainer}>
+                <div className="album-info-container" style={styles.albumInfoContainer}>
                     {/* Display album art (if available) */}
                     {albumArt ? (
                         <img src={albumArt} alt="Album Cover" style={styles.albumArt} />
                     ) : (
                         <img src="/icon1415.png" alt="Default Album Cover" style={styles.albumArt} />
                     )}
+                    <h1 style={{ marginLeft: "0" }}>{albumName}</h1>
+                    <h2 style={{ marginLeft: "0" }}>{artistName}</h2>
+
                 </div>
                 <ul style={styles.tracklist}>
                     {tracks.map((track) => (
-                        <li key={track.id} style={styles.trackItem}>
+                        <li key={track.track_number} style={styles.trackItem}>
                             <button onClick={() => setCurrentSongID(track.id)} style={styles.trackButton}>
-                                {track.title} - {track.artist}
+                                <div style={{ marginRight: "10px", display: "inline-block", width: "30px" }}>
+                                    {track.track_number}
+                                </div>
+                                <div style={{ textAlign: "left", display: "inline-block" }}>
+                                    <div>{track.title}</div>
+                                    {track.artist !== artistName && track.artist !== undefined &&
+                                        // if the artist is different from the album artist, display the rest of the artist name
+                                        // replace all but last, replace last
+                                        <div>{track.artist.replace(artistName + ", ", "").replace(", " + artistName, "")}</div>
+                                    }
+                                </div>
                             </button>
                         </li>
                     ))}
@@ -63,6 +81,7 @@ const styles = {
     container: {
         textAlign: "center",
         padding: "20px",
+        maxWidth: "80%",
         margin: 'auto',
     },
     albumContents: {
@@ -72,14 +91,15 @@ const styles = {
         justifyContent: 'center',
         alignItems: 'center',
     },
-    albumArtContainer: {
+    albumInfoContainer: {
         // don't vertically center align; stick the image at the top
         display: 'flex',
         flexDirection: 'column',
         justifyContent: 'flex-start',
-        alignItems: 'center',
+        // alignItems: 'center',
         margin: '20px',
-
+        // text align to left
+        textAlign: 'left',
     },
     albumArt: {
         width: "500px",
@@ -89,6 +109,7 @@ const styles = {
     tracklist: {
         listStyleType: "none",
         padding: 0,
+        margin: "30px",
         // enable scrolling
         overflowY: 'auto',
         // set the height of the container
@@ -98,7 +119,7 @@ const styles = {
 
     },
     trackItem: {
-        margin: "10px 0",
+        margin: "30px 0",
     },
     trackButton: {
         background: "none",
@@ -107,6 +128,11 @@ const styles = {
         fontSize: "16px",
         cursor: "pointer",
         textAlign: "left",
+        // side by side, left align
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
     },
 };
 
